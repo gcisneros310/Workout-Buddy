f02ca62fccb9020d5cae5d494335e65f1170a3f4
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2022-08-10 12:09:01 -0700

    first commit

---
+++ express-backend/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+ 
+
+// instantiate express
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    // console.log('app.use function running real quick')
+    next()
+})
+
+// retrieves routes from workouts and user
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to MongoDB cluster
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('connected to DB and listening on PORT', process.env.PORT)    
+    })
+})
+.catch((error) => { // catch error in case of bad authentication / failed authentication
+    console.log(error)
+})
+
---
+++ frontend/build/precache-manifest.7df9c7ae5825808bd22cb78ebfbe322b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d1ae0a40f317ebec73",
+    "url": "/Workout-Buddy/static/css/main.e77a71aa.chunk.css"
+  },
+  {
+    "revision": "b5d1ae0a40f317ebec73",
+    "url": "/Workout-Buddy/static/js/main.b5d1ae0a.chunk.js"
+  },
+  {
+    "revision": "2c5e422667d1d30a5d06",
+    "url": "/Workout-Buddy/static/js/1.2c5e4226.chunk.js"
+  },
+  {
+    "revision": "39352cbdc4c65dab7310",
+    "url": "/Workout-Buddy/static/js/runtime~main.39352cbd.js"
+  },
+  {
+    "revision": "fbd52f847f5883ecc2040f171eff4fa0",
+    "url": "/Workout-Buddy/index.html"
+  }
+];
